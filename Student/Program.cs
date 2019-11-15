f7f3ec47e135a335548853b69b3dd1065b23d9ed
thanhson1445 <57778685+thanhson1445@users.noreply.github.com>
2019-11-15 20:21:34 +0700

    NhomCThang, Do Thanh Son, Nguyen Trong Duong, Vu Tri Toan, Nop bai so 2

---
+++ Student/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number Of Student: ");
+            //ctrl + k + c: cmt
+            //ctrl + k + u: uncmt
+            //ctrl + k + d: format code
+            int numOfStudent;
+            try
+            {
+                numOfStudent = int.Parse(Console.ReadLine());
+                InputStudentList(numOfStudent);
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Invalid Input!");
+                throw;
+            }
+            //dung man hinh de xem ket qua
+            Console.ReadKey();
+        }
+        private static void InputStudentList(int numOfStudent)
+        {
+            //tao mang danh sach sinh vien
+            Student[] studentList = new Student[numOfStudent];
+            Student student;
+            for (int i = 0; i < numOfStudent; i++)
+            {
+                student = new Student();
+                Console.Write("ID = ");
+                student.StudentID = int.Parse(Console.ReadLine());
+                //tuong tu
+                //dua doi tuong vao mang
+                studentList[i] = student;
+            }
+        }
+    }
+}
